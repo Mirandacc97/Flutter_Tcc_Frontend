d80af73446451977a299de7259b1c428a72449e0
Felipe Miranda Lima <40402331+Mirandacc97@users.noreply.github.com>
2025-08-03 20:18:20 -0300

    Tela de login funcionando

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.flutter_tcc_frontend"
+    namespace = "com.example.pdv2"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.flutter_tcc_frontend"
+        applicationId = "com.example.pdv2"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -40,5 +40,5 @@ android {
 }
 
 flutter {
-    source = "../.."
+    source = "app/src"
 }
